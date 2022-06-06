53ef6aea7cc8fc559bff9ef19e6e2f0bc6146286
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-06-06 11:25:20 -0400

    init commit

---
+++ security.tf
@@ -0,0 +1,36 @@
+###############################################
+#  AWS SECURITY GROUP FOR EC2 INSTANCES       #
+###############################################
+resource "aws_security_group" "web_and_ssh" {
+  name        = "web_and_ssh"
+  description = "Allow web incgress trafic"
+  vpc_id      = aws_vpc.development.id
+
+  # http port
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ssh port
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Open access to public network
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "concordia-big-data"
+  }
+}
---
+++ ssh_keys.tf
@@ -0,0 +1,11 @@
+###############################################
+# Public Key for accessing EC2 instances      #
+###############################################
+resource "aws_key_pair" "user_key" {
+  key_name   = "user-key"
+  public_key = var.ssh_public_key
+
+  tags = {
+    Name = "concordia-big-data"
+  }
+}
